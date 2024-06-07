a37fcecb6a8b81c4087fc69d6666d518f2876299
Maryna Kamienieva <mkdirwd@gmail.com>
2024-06-07 07:26:03 +0200

    add model Client.cs

---
+++ APBD9_17c/models/Client.cs
@@ -0,0 +1,12 @@
+namespace APBD9_17c.models;
+
+public class Client
+{
+    public int IdClient { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Telephone { get; set; }
+    public string Pesel { get; set; }
+
+}
